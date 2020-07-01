aae042ea77a891b2714cba52a11f7a0c0bb5f790
Mori19 <55312081+Mori19@users.noreply.github.com>
2020-07-01 17:40:07 +1000

    Working commit
    Works reliably, but only wins every 20th game or so. 
    Doesn't matter if you want the steam achievement; leave it on all night or something.

--- ПАСЬЯ́НС/bot.py
+++ ПАСЬЯ́НС/bot.py
@@ -4,8 +4,8 @@ import win32con
 import time
 import numpy as np
 import copy
-PATH = r'path\to\image_array\\'
-FUTURE = 3
+PATH = r'path\to\images\array\\'
+FUTURE = 20
 
 class Bot():
     def __init__(self):
@@ -28,11 +28,11 @@ class Bot():
     def setup_map(self):
         array = []
         for i in range(370,1576,134):
-            for j in range(464,674,30):
+            for j in range(464,704,30):
                 array.append((i,j))
-        print(len(array))
+        #print(len(array))
         for i in range(9):
-            for j in range(7):
+            for j in range(8):
                 self.table_map[i].append(array.pop(0))
 
     
@@ -66,17 +66,20 @@ class Bot():
                 yield i,j
 
     def find_moves(self, this_board,this_special,hypothetical):
-        print(f"the table looks like {this_board}")
+        #if not hypothetical: print(f"the table looks like {this_board}")
         move = None
+        ############################    try to get a card out of the special place, if there is one there   #######
         if this_special:
             depth, this_card = 1, [this_special[0],(1380,227)] #this is a special position. It isnt in the position map. 
             for j in range(9):
                 if len(this_board[j])<2: continue
                 if self.available_move(None,[this_special[0],(0,0)],[this_board[j][-1],(j,len(this_board[j])-1)], None): #I cannot tell why the first and last arguments are required. I dont think they are.
-                    print(f'this card {this_card[0]}, that card {that_card}')
+                    #print(f'this card {this_card[0]}, that card {this_board[j][-1]}')
                     if not hypothetical: self.moves.append(((1380,227),self.table_map[j][len(this_board[j])-1])) 
                     this_board[j].append(this_special.pop(-1))
                     return 0
+                    
+        ############################    move a card onto another card   ###########################################
                 
         if not move:    
             for i,j in self.generate_loop():
@@ -87,38 +90,43 @@ class Bot():
                 move = self.available_move(i,this_card,that_card,depth)
                 if move: 
                     break
-        #do what we can without taking a free column; then take a free column if we must
+        ############################    move a card to a free column    ############################################
         if not move:
             for i, j in self.generate_loop():
                 if len(this_board[i]) == 0: continue
                 this_card = [this_board[i][-1],(i,len(this_board[i])-1)]
+                depth, this_card = self.get_face_depth(i,this_card,this_board) if this_card[0] in self.face else self.get_depth(i,this_card,this_board)
                 if not this_card[0] in self.face and not this_card[0] in ['r10','b10']: continue
-                depth, this_card = self.get_face_depth(i,this_card,this_board) if this_card[0] in self.face else (1, this_card)
                 if len(this_board[i]) == depth: continue
                 if len(this_board[j]) == 0:
                     move = (self.table_map[this_card[1][0]][this_card[1][1]],self.table_map[j][0])
                     break
                     
+        ############################    move a card to the special place    ########################################
         if not move and not this_special:
-            for i,j in self.generate_loop():
+            for i,j in self.generate_loop(): #we'll check if the card we want to move is burried
                 if i == j or len(this_board[i]) < 2 or len(this_board[j]) == 0: continue
                 this_card = [this_board[i][-2],(i,len(this_board[i])-2)]
                 that_card = [this_board[j][-1],(j,len(this_board[j])-1)]
+                depth, discard = self.get_depth(i,[this_board[i][-1],(i,len(this_board[i])-1)],this_board) if this_board[i][-1] in self.numeric else self.get_face_depth(i,[this_board[i][-1],(i,len(this_board[i])-1)],this_board)
+                if depth != 1: continue #this is a stupid loop where we cycle putting the card on the special place and take it back off lol
                 if self.available_move(i,this_card,that_card,depth):
-                    print(f'this card {this_card[0]}, special')
-                    if not hypothetical: self.moves.append((self.table_map[i][-1],(1380,227)))
+                    #print(f'this card {this_card[0]}, special')
+                    if not hypothetical: self.moves.append((self.table_map[i][len(this_board[i])-1],(1380,227)))
                     this_special.append(this_board[i].pop(-1))
-                    print("no error")
+                    #print("no error")
                     return 0
-            for i,j in self.generate_loop():
+            for i,j in self.generate_loop(): #we'll check if the card we want to place ours on is burried
                 if i == j or len(this_board[i]) == 0 or len(this_board[j]) < 2: continue
                 this_card = [this_board[i][-1],(i,len(this_board[i])-1)]
                 that_card = [this_board[j][-2],(j,len(this_board[j])-2)]
+                depth, discard = self.get_depth(j,[this_board[j][-1],(j,len(this_board[j])-1)],this_board) if this_board[j][-1] in self.numeric else self.get_face_depth(j,[this_board[j][-1],(j,len(this_board[j])-2)],this_board)
+                if depth != 1: continue
                 if self.available_move(i,this_card,that_card,depth):
-                    print(f'that card {that_card[0]}, special')
-                    if not hypothetical: self.moves.append((self.table_map[j][-1],(1380,227))) 
+                    if not hypothetical: print(f'that card {that_card[0]}, special')
+                    if not hypothetical: self.moves.append((self.table_map[j][len(this_board[j])-1],(1380,227))) 
                     this_special.append(this_board[j].pop(-1))
-                    print("no error")
+                    #print("no error")
                     return 0
 
         if (not hypothetical) and move: 
@@ -132,18 +140,20 @@ class Bot():
             elif not hypothetical and not passed:
                 print("no future")
                 return 2
-            print("no error")
+            #print("no error")
             return 0
-        print("not found")
+        #print("not found")
         return 1
     
     def test_future(self):
         test_table = copy.deepcopy(self.table)
         test_special = copy.deepcopy(self.special_place)
+        print(f'did we win {self.test_win(test_table)}')
         for i in range(FUTURE):
-            print(f"Test {i}")
-            if self.find_moves(test_table,test_special,True):
-                if not self.test_win(test_table): return False
+            #print(f'did we win hypothetically? {self.test_win(test_table)}')
+            if self.test_win(test_table): return True
+            #print(f"Test {i}")
+            if self.find_moves(test_table,test_special,True): return False
         return True
         
     def available_move(self,i,this_card,that_card,depth):
@@ -188,43 +198,53 @@ class Bot():
         
         
     def test_win(self,test_table):
-        win = []
-        win.append(['heart','heart','heart','heart'] in test_table)
-        win.append(['diamond','diamond','diamond','diamond'] in test_table)
-        win.append(['spade','spade','spade','spade'] in test_table)
-        win.append(['club','club','club','club'] in test_table)
-        win.append(['r10','r9','r8','r7','r6'] in test_table)
-        win.append(['b10','b9','b8','b7','b6'] in test_table)
+        mod_table = copy.deepcopy(test_table)
+        for i in range(9):
+            if mod_table[i] == ['heart' for i in range(4)]:
+                mod_table[i].clear()
+            if mod_table[i] == ['diamond' for i in range(4)]:
+                mod_table[i].clear()
+            if mod_table[i] == ['club' for i in range(4)]:
+                mod_table[i].clear()
+            if mod_table[i] == ['spade' for i in range(4)]:
+                mod_table[i].clear()
+            if mod_table[i] == ['r10','b9','r8','b7','r6']:
+                mod_table[i].clear()
+            if mod_table[i] == ['b10','r9','b8','r7','b6']:
+                mod_table[i].clear()
+        print(f'{mod_table}')
+        win = [not bool(mod_table[i]) for i in range(9)]
         return all(win)
 
 
     def enact_move(self,pos_map): #move looks like [(x,y),(x2,y2)]
         win32api.SetCursorPos(pos_map[0])
-        time.sleep(0.5)
+        time.sleep(0.7)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,pos_map[0][0],pos_map[0][1])
-        time.sleep(0.5)
+        time.sleep(0.7)
         win32api.SetCursorPos(pos_map[1])
-        time.sleep(0.5)
+        time.sleep(0.7)
         win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,pos_map[1][0],pos_map[1][1])
-        time.sleep(0.5)
+        time.sleep(0.7)
     
     def main(self):
-        for games in range(1):
+        for games in range(1000):
             self.moves = []
             self.populate_table()
             errormap = ["no error","no move found","cant see enough future moves"]
-            for i in range(30):
-            #while not self.find_moves(self.table,False):
-                #pass
-                print(f"Move number {i}")
-                print(errormap[self.find_moves(self.table,self.special_place, False)])
+            #for i in range(40):
+            while not self.find_moves(self.table,self.special_place,False):
+                pass
+                #print(f"Move number {i}")
+                #print(errormap[self.find_moves(self.table,self.special_place, False)])
             #print(self.moves)
             time.sleep(2)
-            print(f'there are {len(self.moves)} moves')
-            while self.moves:
-                self.enact_move(self.moves.pop(0))
+            print(f'it is game {games}, and there are {len(self.moves)} moves')
+            if self.test_win(self.table):
+                while self.moves:
+                    self.enact_move(self.moves.pop(0))
             print(f'did we win? {self.test_win(self.table)}')
-            #self.new_game(pause)
+            self.new_game(1)
             
     def new_game(self,pause):
         time.sleep(pause)
